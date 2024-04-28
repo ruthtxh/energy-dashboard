233b89698c8da9093b06949f29349a81377ebcb4
Ruth Toh <ruthtoh@live.com.sg>
2024-04-28 20:00:38 +0800

    add leaflet map with markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,32 +3,46 @@ import { CommonModule } from '@angular/common';
 import { WeatherLocationComponent } from '../weather-location/weather-location.component';
 import { WeatherLocation } from '../weatherlocation';
 import { WeatherService } from '../weather.service';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, LatLng, tileLayer, marker, icon, Layer } from 'leaflet';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule, WeatherLocationComponent],
-  template: `
-  <section>
-  <form>
-    <input type="text" placeholder="Filter by city" />
-    <button class="primary" type="button">Search</button>
-  </form>
-</section>
-<section class="results">
-  <app-weather-location *ngFor="let weatherLocation of weatherLocationList"
-  [weatherLocation]="weatherLocation"></app-weather-location>
-</section>
-  `,
+  imports: [CommonModule, WeatherLocationComponent, LeafletModule],
+  templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent {
   weatherLocationList: WeatherLocation[] = [];
   weatherService: WeatherService = inject(WeatherService);
+  layers: Layer[] = [];
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 10,
+    center: latLng(1.375, 103.839)
+  };
   constructor() {
     this.weatherService.getAllWeatherLocations().then((weatherLocationList: WeatherLocation[]) => {
       this.weatherLocationList = weatherLocationList;
+      this.weatherLocationList.forEach(el => {
+        const newMarker = marker(
+          [el.label_location.latitude, el.label_location.longitude],
+          {
+            icon: icon({
+              iconSize: [25, 41],
+              iconAnchor: [13, 41],
+              iconUrl: 'leaflet/marker-icon.png',
+              iconRetinaUrl: 'leaflet/marker-icon-2x.png',
+              shadowUrl: 'leaflet/marker-shadow.png'
+            })
+          }
+        )
+        this.layers.push(newMarker)
+      })
     });
+
   }
 }
-
