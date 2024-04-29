f501cc814ec86c114b9296fc8607a1e6fe77988d
Ruth Toh <ruthtoh@live.com.sg>
2024-04-29 15:43:29 +0800

    add marker cluster to map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MapComponent],
+  imports: [ MapComponent],
   template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
-        <app-map></app-map>
+        <markercluster-demo></markercluster-demo>
       </section>
     </main>
   `,
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,48 +1,83 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { WeatherLocationComponent } from '../weather-location/weather-location.component';
 import { WeatherLocation } from '../weatherlocation';
 import { WeatherService } from '../weather.service';
+// Must import this before the modules
+import * as L from 'leaflet';
+import 'leaflet.markercluster';
+import { Control } from 'leaflet';
+import LayersOptions = Control.LayersOptions;
+import { LeafletMarkerClusterModule } from '@asymmetrik/ngx-leaflet-markercluster';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { latLng, LatLng, tileLayer, marker, icon, Layer } from 'leaflet';
 
 @Component({
-  selector: 'app-map',
-  standalone: true,
-  imports: [CommonModule, WeatherLocationComponent, LeafletModule],
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css'],
+	selector: 'markercluster-demo',
+	standalone: true,
+	imports: [LeafletModule,
+		LeafletMarkerClusterModule,],
+	templateUrl: './map.component.html'
 })
-export class MapComponent {
-  weatherLocationList: WeatherLocation[] = [];
-  weatherService: WeatherService = inject(WeatherService);
-  layers: Layer[] = [];
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 10,
-    center: latLng(1.375, 103.839)
-  };
-  constructor() {
-    this.weatherService.getAllWeatherLocations().then((weatherLocationList: WeatherLocation[]) => {
-      this.weatherLocationList = weatherLocationList;
-      this.weatherLocationList.forEach(el => {
-        const newMarker = marker(
-          [el.label_location.latitude, el.label_location.longitude],
-          {
-            icon: icon({
-              iconSize: [25, 41],
-              iconAnchor: [13, 41],
-              iconUrl: 'leaflet/marker-icon.png',
-              iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-              shadowUrl: 'leaflet/marker-shadow.png'
-            })
-          }
-        )
-        this.layers.push(newMarker)
-      })
-    });
 
-  }
+export class MapComponent implements OnInit {
+
+	// Weather location info from service
+	weatherLocationList: WeatherLocation[] = [];
+	weatherService: WeatherService = inject(WeatherService);
+
+	// Open Street Map Definition
+	LAYER_OSM = {
+		id: 'openstreetmap',
+		name: 'Open Street Map',
+		enabled: false,
+		layer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		
+			attribution: 'Open Street Map'
+		}),
+	};
+
+	// Values to bind to Leaflet Directive
+	layersControlOptions: LayersOptions = { position: 'bottomright' };
+	baseLayers = {
+		'Open Street Map': this.LAYER_OSM.layer
+	};
+	options = {
+		zoom: 10,
+		center: L.latLng(1.375, 103.839)
+	};
+
+	// Marker cluster stuff
+	markerClusterGroup: L.MarkerClusterGroup = new L.MarkerClusterGroup();
+	markerClusterData: L.Marker[] = [];
+	markerClusterOptions: L.MarkerClusterGroupOptions = {};
+
+	ngOnInit() {
+		this.refreshData();
+	}
+
+	markerClusterReady(group: L.MarkerClusterGroup) {
+		this.markerClusterGroup = group;
+	}
+
+	refreshData(): void {
+		const data: L.Marker[] = [];
+		this.weatherService.getAllWeatherLocations().then((weatherLocationList: WeatherLocation[]) => {
+			this.weatherLocationList = weatherLocationList;
+			this.weatherLocationList.forEach(el => {
+				const newMarker = L.marker(
+					[el.label_location.latitude, el.label_location.longitude],
+					{
+						icon: L.icon({
+							iconSize: [25, 41],
+							iconAnchor: [13, 41],
+							iconUrl: 'leaflet/marker-icon.png',
+							iconRetinaUrl: 'leaflet/marker-icon-2x.png',
+							shadowUrl: 'leaflet/marker-shadow.png'
+						})
+					}
+				)
+				data.push(newMarker)
+			})
+			this.markerClusterData = data;
+		});
+	}
 }
