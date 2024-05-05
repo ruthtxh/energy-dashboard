81cd4d2dcb65065627b2adada51ed05acc7ce261
Ruth Toh <ruthtoh@live.com.sg>
2024-05-05 18:04:02 +0800

    add nitty gritty subscripts

--- src/app/map/search-summary/search-summary.component.ts
+++ src/app/map/search-summary/search-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IrradiationChartComponent } from 'src/app/shared/irradiation-chart/irradiation-chart.component';
 
@@ -12,7 +12,37 @@ import { IrradiationChartComponent } from 'src/app/shared/irradiation-chart/irra
     height: calc(100%);
   }`
 })
-export class SearchSummaryComponent {
+export class SearchSummaryComponent implements OnInit {
   @Input() filteredLat?: number;
   @Input() filteredLong?: number;
+
+  lastUpdateDateTime: string = "";
+
+
+  ngOnInit() {
+    this.lastUpdateDateTime = getFormatDateTime()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.lastUpdateDateTime = getFormatDateTime()
+  }
+}
+
+function getFormatDateTime() {
+  const date = new Date();
+  return `${date.toLocaleDateString('en-GB', {
+    day: 'numeric', month: 'short', year: 'numeric'
+  })}    ${formatAMPM(date)}`;
+}
+
+function formatAMPM(date: Date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  var strMinutes = minutes.toString();
+  if (minutes < 10) strMinutes = `0${minutes}`;
+  var strTime = hours + ':' + strMinutes + ' ' + ampm;
+  return strTime;
 }
